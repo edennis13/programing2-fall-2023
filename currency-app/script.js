a9770afadcbec3aa3053fe0ce20666ae610ce367
Ethan Dennis <ethan@Ethans-MacBook-Pro.local>
2023-11-13 13:52:03 -0600

    currency continued

--- currency-app/script.js
+++ currency-app/script.js
@@ -1,26 +1,58 @@
-let usd = parseInt(prompt("What ammout of USD would you like to convert?"));
 
-let usdSpan = document.getElementById("usd");
-usdSpan.innerText = usd;
+function convert(){
+	// let usd = parseInt(prompt("What ammout of USD would you like to convert?"));
+	let usd = document.getElementById("currency-amount").value;
+	let usdSpan = document.getElementById("usd");
+	let currency = document.getElementById("currencies").value;
+	let convertedValueDiv = document.getElementById("converted-value");
+	// usdSpan.innerText = usd;
 
+console.log(currency);
 
-let eur = (usd*.94)
-let eurSpan = document.getElementById("eur");
-eurSpan.innerText = eur;
+if (currency == "eur"){
+	let newAmount = usd*0.94;
+	convertedValueDiv.innerText = newAmount;
 
-let gbp = (usd*.82)
-let gbpSpan = document.getElementById("gbp");
-gbpSpan.innerText = gbp;
+} 
+else if(currency = "gbp"){
+	let newAmount = usd*0.82;
+	convertedValueDiv.innerText = newAmount;
+}
+else if(currency = "cad"){
+	let newAmount = usd*0.38;
+	convertedValueDiv.innerText = newAmount;
+}
+else if(currency = "chf"){
+	let newAmount = usd*0.90;
+	convertedValueDiv.innerText = newAmount;
+}
+else if(currency = "jpy"){
+	let newAmount = usd*152.52;
+	convertedValueDiv.innerText = newAmount;
+}
+else{
+	
+}
 
-let cad = (usd*1.38)
-let cadSpan = document.getElementById("cad");
-cadSpan.innerText = cad;
+// console.log(newAmount)
 
-let chf = (usd*.90)
-let chfSpan = document.getElementById("chf");
-chfSpan.innerText = chf;
+	// let eur = (usd*.94)
+	// let eurSpan = document.getElementById("eur");
+	// eurSpan.innerText = eur;
 
-let jpy = (usd*152.52)
-let jpySpan = document.getElementById("jpy");
-jpySpan.innerText = jpy;
+	// let gbp = (usd*.82)
+	// let gbpSpan = document.getElementById("gbp");
+	// gbpSpan.innerText = gbp;
 
+	// let cad = (usd*1.38)
+	// let cadSpan = document.getElementById("cad");
+	// cadSpan.innerText = cad;
+
+	// let chf = (usd*.90)
+	// let chfSpan = document.getElementById("chf");
+	// chfSpan.innerText = chf;
+
+	// let jpy = (usd*152.52)
+	// let jpySpan = document.getElementById("jpy");
+	// jpySpan.innerText = jpy;
+}
