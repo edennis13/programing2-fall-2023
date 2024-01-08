c1a861275d314cd4d2d66f6482542ac26fe7efdc
Ethan Dennis <ethan@Ethans-MacBook-Pro.local>
2024-01-08 15:46:00 -0600

    page 1 almost

---
+++ ssdp/script.js
@@ -0,0 +1,8 @@
+var btn = document.getElementbyId("btn");
+btn.addEventListener("click", function(e){
+	var name = document.getElementbyId("name").value;
+	var email = document.getElementbyId("email").value;
+	var subject = document.getElementbyId("subject").value;
+	var message = document.getElementbyId("message").value;
+
+})
